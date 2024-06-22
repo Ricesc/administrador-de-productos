fd14dbed30f66ae5d4c133904f8f05af4a39f604
Ricesc <ricesc14@gmail.com>
2024-06-22 05:45:02 -0400

    Administrador de Productos I

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import ProductForm from "../ProductForm/ProductForm";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <ProductForm />
+        </div>
+    );
+};
+
+export default App;
